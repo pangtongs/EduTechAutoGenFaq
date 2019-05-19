c86842d3cf89fccf2f55370c2772944d510de889
Winston Andersen <pangtongwushu@gmail.com>
2019-05-19 14:07:57 +0700

    fixes indentation problem in reddit calling API

--- Scripts/src/experiment.py
+++ Scripts/src/experiment.py
@@ -27,12 +27,13 @@
 import praw
 import config
 
-reddit = praw.Reddit(client_id = config.client_id,
-                     client_secret= config.client_secret,
-                    username=config.username,
-                    password=config.password,
-                     user_agent=config.user_agent
-                     )
+reddit = praw.Reddit(
+  client_id = config.client_id,
+  client_secret= config.client_secret,
+  username=config.username,
+  password=config.password,
+  user_agent=config.user_agent
+)
 subreddit=reddit.subreddit('OMSCS')
 hot_python = subreddit.hot(limit=500)
 
